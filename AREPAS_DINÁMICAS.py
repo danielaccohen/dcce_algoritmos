132abb7d99a4495d261cac1928bafc7b35c74b47
danielaccohen <156797461+danielaccohen@users.noreply.github.com>
2024-01-24 21:16:32 -0400

    arepas dinámicas

---
+++ AREPAS_DINÁMICAS.py
@@ -0,0 +1,6 @@
+print("Hagamos arepas!")
+harina=int(input("Cuántas tazas quieres usar de harina pan?"))
+agua=harina*0.25
+sal=harina*0.5
+print("Para preparar arepas con " +str(harina)+ " tazas de harina pan, vas necesitar " +str(agua)+ " tazas de agua y "+str(sal)+ " cucharadita(s) de sal.")
+quit()
